73f602a68d77c30ff012c231910ef5b7acbef049
joyhouses <joyhouse494@gmail.com>
2026-06-01 02:25:22 +0900

    chore: update astro.site

--- astro.config.js
+++ astro.config.js
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  // site: "joyhouses.github.io",
+  site: "joyhouses.github.io",
   vite: {
     plugins: [tailwindcss()],
   },
